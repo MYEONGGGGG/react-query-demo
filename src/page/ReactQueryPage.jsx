b23727ea14da3b912fd673d762a2e999e166796c
myeongeun <c_me95@mentorit.co.kr>
2024-05-12 23:30:45 +0900

    react query api 호출 통제 학습

--- src/page/ReactQueryPage.jsx
+++ src/page/ReactQueryPage.jsx
@@ -12,7 +12,11 @@ const ReactQueryPage = () => {
         queryKey: ['posts'],
         queryFn: fetchPost,
         retry: 1,
-        select: (data) => { return data.data }
+        staleTime: 60000, // 1분간 api 호출 금지
+        gcTime: 10000, // 10초간 캐시 유지
+        // stale < gctime 로 시간을 설정하는게 좋다.
+        // 이유? 캐시가 유지되는 시간이 짧으면 또 다시 api 를 빈번하게 요청해야하며, 로딩 화면을 반복적으로 노출해야하기때문
+        select: (data) => { return data.data },
     });
 
     // console.log('success: ', data, isLoading);
